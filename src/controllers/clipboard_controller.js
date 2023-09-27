02071f985aefc9254b4676d3772a63856c513477
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-27 09:43:02 +0200

    Add copy link

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['source']
+  static targets = ['source'];
 
   copy() {
-    navigator.clipboard.writeText(this.sourceTarget.value)
+    event.preventDefault();
+    navigator.clipboard.writeText(this.sourceTarget.value);
   }
 }
