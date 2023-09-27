6fd7c603dcc598e1620ef7df08b239205c501238
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-27 16:12:29 +0200

    Set a timer to fetch /messages.html every 5 seconds

--- src/controllers/content_loader_controller.js
+++ src/controllers/content_loader_controller.js
@@ -3,10 +3,21 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static values = {
     url: String,
+    refreshInterval: Number,
   };
+
+  startRefreshing() {
+    setInterval(() => {
+      this.load();
+    }, this.refreshIntervalValue);
+  }
 
   connect() {
     this.load();
+
+    if (this.hasRefreshIntervalValue) {
+      this.startRefreshing();
+    }
   }
 
   load() {
