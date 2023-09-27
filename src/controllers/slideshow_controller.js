c053e6dac489d6afb5c077eb4e371d5193e59764
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-27 14:19:51 +0200

    Add slideshow controller

---
+++ src/controllers/slideshow_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['slide'];
+
+  initialize() {
+    this.index = 0;
+    this.showCurrentSlide();
+  }
+
+  next() {
+    this.index++;
+    this.showCurrentSlide();
+  }
+
+  previous() {
+    this.index--;
+    this.showCurrentSlide();
+  }
+
+  showCurrentSlide() {
+    this.slideTargets.forEach((element, index) => {
+      console.log(`index: ${index}`);
+      element.hidden = index !== this.index;
+    });
+  }
+}
